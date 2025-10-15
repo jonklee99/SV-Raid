02e8aa4aa7dd942dac540e92474a9c42db1f300a
hexbyt3 <hexbyt3@users.noreply.github.com>
2025-10-14 20:45:49 -0500

    Version Bump

--- SysBot.Pokemon/Helpers/SVRaidBot.cs
+++ SysBot.Pokemon/Helpers/SVRaidBot.cs
@@ -2,7 +2,7 @@
 {
     public static class SVRaidBot
     {
-        public const string Version = "v8.7.1";
+        public const string Version = "v8.7.2";
         public const string Repo = "https://github.com/hexbyt3/SVRaidBot";
         public const string ConfigPath = "config.json";
     }
